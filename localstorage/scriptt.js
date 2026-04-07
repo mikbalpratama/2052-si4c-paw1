9e27d07ac5612a1d55ecc3660a3392787fd88149
mikbalpratama <muhamadikbalpratama_2428240135@mhs.mdp.ac.id>
2026-04-07 13:07:28 +0700

    MENAMBAHKAN UNTUK NAMPILKAN GAMBAR DI FORM MAHASISWA

--- localstorage/scriptt.js
+++ localstorage/scriptt.js
@@ -1,5 +1,6 @@
 let npm = document.getElementById("npm")
 let nama = document.getElementById("nama")
+let image = document.getElementById("image")
 
 function simpan() {
     console.log(npm.value)
@@ -23,7 +24,8 @@ function simpan() {
     // simpan balue npm dan nama ke dalam objek data
     data.push( {
         npm: npm.value,
-        nama: nama.value
+        nama: nama.value,
+        image: image.value
     })
     console.log(data)
 
@@ -46,7 +48,16 @@ function tampil(){
     hasil.forEach(element => {
         console.log(element)
 
-        document.getElementById("list-mhs").innerHTML += `<li>${element.npm} ${element.nama}</li>`
+        document.getElementById("list-mhs").innerHTML += 
+        `<div class="col-lg-4 col-md-6 mb-3">
+            <div class="card p-2">
+                <img src="${element.image}" class="card-img-top" img-fluid">
+                <div class="card-body">
+                    <h4 class="text-primary" > ${element.npm} </h4>
+                    <h6 class="text-danger" > ${element.nama} </h6>
+                </div>
+            </div>
+        </div>`
         
     });
 }
